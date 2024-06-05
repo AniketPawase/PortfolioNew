7f1691866c1228f8a45868f7b515da7b30014806
AniketPawase <138360404+AniketPawase@users.noreply.github.com>
2024-06-05 14:56:21 +0530

    updated Resume

--- src/component/Home.js
+++ src/component/Home.js
@@ -42,7 +42,7 @@ function Home() {
           </p>
           <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
             <a
-              href="https://drive.google.com/file/d/1PTSM7ya4WdzR-zp5rcXjDK_3RkYsxMrR/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1FzMJo-oX_cKYsWOaXnBgrQh5BoIzwXCN/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               className="text-decoration-none btn btn-outline-primary btn-lg px-4 me-md-2 fw-bold"
