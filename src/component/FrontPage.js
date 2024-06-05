b5680686af62219ca1c50c2929abca07729364d3
AniketPawase <138360404+AniketPawase@users.noreply.github.com>
2024-06-05 12:26:13 +0530

    updated Resume and Experience

--- src/component/FrontPage.js
+++ src/component/FrontPage.js
@@ -4,12 +4,14 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 import Skills from "./Skills";
 import Contact from "./contact";
+import Experience from './Experience';
 function FrontPage() {
   return (
     <div>
       <Navbar />
       <Home />
       <Skills />
+      <Experience/>
       <Project />
       <Education />
       <Contact />
